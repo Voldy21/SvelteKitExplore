714fe6d697c7fdfdec737a8d7ab639c2f7e052f8
Jonah Livingston <you@example.com>
2023-02-11 19:28:05 -0500

    added counter

---
+++ src/routes/+page.svelte
@@ -0,0 +1,46 @@
+<script>
+    import Counter from './components/Nested.svelte';
+
+</script>
+
+<div class="body">
+    
+    <div class="content">
+        <div class="wrapper">
+            <Counter />
+        </div>
+        <div class="wrapper">
+            
+        </div>
+    </div>
+</div>
+
+<style>
+    
+.body {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    background-color: #8458B3;
+}
+
+.content {
+    background-color: white;
+    padding: 20px;
+    border-radius: 25px;
+    text-align: center;
+    width: 80%;
+    max-width: 800px;
+    border: 2px solid black;
+}
+
+.wrapper {
+  margin: 40px 0;
+  border-bottom: 1px solid black;
+  padding-bottom: 20px;
+}
+
+
+
+</style>
---
+++ src/routes/components/Nested.svelte
@@ -0,0 +1,93 @@
+<script>
+    let count = 0;
+    let rounded = false;
+
+    function floorHalve(){
+        if(count % 2 == 1){
+        rounded = true;
+        setTimeout(()=>rounded=false, 1000);
+    }
+
+    count = Math.round( count / 2)
+    }
+
+    function Halve(){
+        count /= 2;
+    }
+</script>
+
+<button on:click={() => count++}>
+    Increment
+</button>
+
+<button on:click={() => count--}>
+    Decrement
+</button>
+
+<button on:click={() => count*=2}>
+    Double
+</button>
+
+<button on:click={Halve}>
+    Halve
+</button>
+
+<button on:click={floorHalve}>
+    Floor the half
+</button>
+
+<div class="center">
+    <div class="Rounded">
+        {#if rounded}
+            <span>Rounded</span>
+        {/if}
+    </div>
+
+</div>
+
+
+
+<p>Clicked {count} {count === 1 ? 'time' : "times"}</p>
+
+<style>
+    p{
+        font-family: Arial, Helvetica, sans-serif;
+    }
+
+    button {
+        background-color: blue;
+        color: white;
+        border-radius: 25px;
+        border: none;
+        padding: 10px 20px;
+        font-size: 16px;
+        cursor: pointer;
+        transition: all 0.3s ease;
+    }
+
+    button:hover {
+        background-color: darkblue;
+    }
+
+    button:active {
+        transform: translateY(2px);
+    }
+
+    .Rounded {
+        display:flex;
+        justify-content: center;
+        font-weight: bold;
+        font-size: 1.3rem;
+        padding-top: 10px;
+        height: 30px;
+        width: 85px
+}
+    .Rounded:after {
+        content: '\200b';
+    }
+
+    .center {
+        display: flex;
+        justify-content: center;
+    }
+</style>
